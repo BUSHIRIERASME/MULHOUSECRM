9b9f58b519557a8e1c19e3d25c36c9cc6c3b3091
BUSHIRIERASME <85500868+BUSHIRIERASME@users.noreply.github.com>
2021-06-08 10:51:17 +0200

    la moyenne
    calcul de la moyenne de 2 nombres

--- src/moyenne/App.java
+++ src/moyenne/App.java
@@ -1,14 +1,25 @@
 package moyenne;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args)
      {
-        int a=5;
-        int b=12;
-        float c;
-        float c = a+b;
-        System.out.println("Saisie la somme est"+ c);
+        int a;
+        int b;
+        Scanner scanner;
+        float resultat;
+        System.out.println("calcle de la moyenne de nombres:");
+        scanner =new Scanner (System.in);
+        System.out.println("veillez saisir un nombre:");
+        a = scanner. nextInt();
+        System.out.println("veillez saisir un nombre:");
+        b = scanner. nextInt();
+        resultat = (float)a + (float)b / 2f;
+        System.out.println("Le resultat est:"+ resultat);
+        scanner.close();
+
     }
     
 }
