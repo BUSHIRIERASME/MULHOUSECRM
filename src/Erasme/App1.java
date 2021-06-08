e6a3e8aecf6a97cefdf9674535cf6884b9e1f1c8
BUSHIRIERASME <85500868+BUSHIRIERASME@users.noreply.github.com>
2021-06-08 09:49:01 +0200

    Create App1.java

---
+++ src/Erasme/App1.java
@@ -0,0 +1,15 @@
+package Erasme;
+
+public class App1
+{
+
+    public static void main(String[] args)
+    {
+         
+        
+        System.out.println("Bonjour Erasme");
+        System.out.println("Quel est le programme du jour");
+        System.out.println("La pêche");
+    } 
+    
+}
