fe99e46994b8c15c103e16a451395a55ba5cc5c1
BUSHIRIERASME <erasme.bushiri@hotmail.fr>
2021-06-16 12:17:50 +0200

    Update Kilometre.java

--- src/Kilometres1/Kilometre.java
+++ src/Kilometres1/Kilometre.java
@@ -6,15 +6,21 @@ public class Kilometre
 {
   public static void main(String[] args) 
   {
-      int valeur;
-      String Q;
+      int saisie = 0;
+      int Q = 0 ;
       Scanner scanner;
       scanner = new Scanner(System.in);
-      System.out.println();
-      System.out.println("Saisie une valeur");
-      valeur = scanner.next();
-      String saisie = scanner.Next();
-     if(valeur=Q);
+      System.out.println("Saisie une valeur en KM");
+      saisie = scanner.nextInt();
+       saisie = scanner.nextInt();
+     if(saisie <= 0,01  >= 1000000);
+       {
+         System.out.println(" Saisie hors e prté, veuillez en saisir un autre");
+         saisie = saisie*1.609;
+         System.out.println("La reponse en miles est : " +saisie);
+        
+       }
+  
 
       
 
