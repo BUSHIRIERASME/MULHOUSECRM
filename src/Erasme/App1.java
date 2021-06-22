c4452c054d8888f677b67528e8fab023979965d6
BUSHIRIERASME <85500868+BUSHIRIERASME@users.noreply.github.com>
2021-06-22 13:00:03 +0200

    travail jour220621

--- src/Erasme/App1.java
+++ src/Erasme/App1.java
@@ -6,13 +6,12 @@ public class App1
     public static void main(String[] args)
     {
         String ville ="marseille";
-        System.out.printl("Bonjour Erasme");
-        System.out.println("Quel est le programme du jour");
-        System.out.println("La pêche à " + ville + ".");
-
-        ville = "Lille";
         
-        System.out.println("La pétanque à " + ville + ".");
+        System.out.println("Bonjour Erasme");
+        System.out.println("Quel est le programme du jour");
+        System.out.println("La pêche à " + ville);
+        ville = "Lille";
+        System.out.println("La pétanque à " + ville );
     } 
     
 }
--- src/Nombrepremier/nombrepremier1.java
+++ src/Nombrepremier/nombrepremier1.java
@@ -2,27 +2,34 @@ package Nombrepremier;
 
 import java.util.Scanner;
 
+import somme1.somme;
+
 public class nombrepremier1
  {
-   public static void main(String[] args) {
-       int N = 100;
-       int i = 2;
+   public static void main(String[] args) 
+      {
+       int N ;
+       int diviseur = 2;
+       int somme;
        Scanner scanner;
        scanner = new Scanner(System.in);
       
        System.out.println("saisie un nombre");
        N = scanner.nextInt();
       
-       for(i=2; 1<100 ; i++) 
+       for(diviseur=2; diviseur <= N/2 ; diviseur++) 
+       
+          if(somme == 0) ( stop = false ; break;)
+          
+
        //if (N % 1 == 0); 
         { 
        System.out.println(N + " est un nombre premier");
-       
+        }
        N = scanner.nextInt();
-       Scanner scaner;
-       }
-       scaner.close();
+       scanner.close();
+      
+      }
 
-
-   } 
-}
+      
+  }       
