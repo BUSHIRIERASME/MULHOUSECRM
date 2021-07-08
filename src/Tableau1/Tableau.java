c4d2d52196e9e1e118f5d1025144d3afe1a625f8
BUSHIRIERASME <85500868+BUSHIRIERASME@users.noreply.github.com>
2021-07-08 12:55:08 +0200

    Create Tableau.java

---
+++ src/Tableau1/Tableau.java
@@ -0,0 +1,29 @@
+package Tableau1;
+
+public class Tableau 
+{
+ public static void main(String[]args)
+ {;
+    int i = 0;
+   
+    String []tableau = new String [11];
+   
+    tableau[0] = "Orange";
+    tableau[1] = "Citron";
+    tableau[2] = "Clementine";
+    tableau[3] = "Kiwi";
+    tableau[4] = "Banane";
+    tableau[5] = "Mangue";
+    tableau[6] = "Péche";
+    tableau[7] = "Abricot";
+    tableau[8] = "Fraise";
+    tableau[9] = "Kaki";
+    tableau[10]= "Romarin";
+    
+    for(int i= 0; i<tableau.lenght; i++)
+ }
+ {
+     System.out.println();
+ }
+}
+
