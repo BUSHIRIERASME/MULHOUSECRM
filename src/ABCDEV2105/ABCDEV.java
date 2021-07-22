4f9de0af7cd57279a055149e05ae79935fd32159
BUSHIRIERASME <85500868+BUSHIRIERASME@users.noreply.github.com>
2021-07-22 10:24:50 +0200

    T220721

---
+++ src/ABCDEV2105/ABCDEV.java
@@ -0,0 +1,27 @@
+package ABCDEV2105;
+
+public class ABCDEV
+{
+    public static void main(String[]args)
+     {
+         int i=0;
+         String[] tableau = new String [14];
+         tableau[0]=" Devoldere Michaël le maestro";
+         tableau[1]="Albrecht Denbis";
+         tableau[2]="Bardon Xavier";
+         tableau[3]="Bouller Axel";
+         tableau[4]="Bourguignon kevin";
+         tableau[5]="Bushiri Ernest";
+         tableau[6]="Dekkoumi Sabah";
+         tableau[7]="Dubosclard Michaêl";
+         tableau[8]="Geniesse Mathieu";
+         tableau[9]="Legrand Cyril";
+         tableau[10]="Milletre Julien";
+         tableau[11]="Ribault Oceane";
+         tableau[12]="Simon Chautemp Damien";
+         tableau[13]="Turleque Fabrique";
+         for(i=0; i<tableau.length;i++)
+         {
+             System.out.println(tableau[i]);
+         }
+}   }
