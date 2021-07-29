a5fb91e118556dce9c9bcd2b9723ca656c57b466
BUSHIRIERASME <85500868+BUSHIRIERASME@users.noreply.github.com>
2021-07-29 16:42:16 +0200

    T290721

---
+++ src/EVALUATION/Document.java
@@ -0,0 +1,33 @@
+package EVALUATION;
+
+
+
+public class Document
+ {
+     public static void main(string[]args)
+     { 
+    private String authors;
+    private Date date;
+    public String getAuthors();
+    public void addAuthors(Name);
+    public Date date() {
+        
+    }
+}
+
+
+public class Book extends Document
+{
+private String title;
+public String getTitle();
+}
+}
+
+public class EMail extends Document
+{
+    private String subjet;
+    private string[] to;
+    public String getSubjet();
+    public string getTo();
+}
+}
