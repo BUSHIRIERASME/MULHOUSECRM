dc95bc8381911d8ed2fa7d8313192f88703f31ae
BUSHIRIERASME <85500868+BUSHIRIERASME@users.noreply.github.com>
2021-09-08 16:38:14 +0200

    T080821

---
+++ maisonvirtuellejava/Maisonvirtuelle/Portedegarage.java
@@ -0,0 +1,64 @@
+package Maisonvirtuelle;
+
+public class Portedegarage
+
+{
+    // declaration des attributs
+    double ouvertureEnPourcentage;
+    boolean verouille;
+    //etc
+
+    public Portedegarage(){
+    
+        ouvertureEnPourcentage=0;
+        verouille=true;
+    }
+     // declaration du constructeur
+    public double getOuvertureEnPourcentage()
+    {
+       return 0; 
+    }
+
+   /* public void ouvertureEnPourcentage()
+    {
+       System.out.println( this.portegarage + "s'ouvre à 100%");
+        System.out.println( this.portegarage + "s'ouvre à 50%");
+        System.out.println( this.portegarage + "s'ouvre à 11.11%");
+        System.out.println( this.portegarage + "s'ouvre à 0%");
+    }
+        public void ferme()
+    {
+        System.out.println(this.portegarage + "se ferme à 100%");
+        System.out.println(this.portegarage + "se ferme à 50%");
+        System.out.println(this.portegarage + "se ferme à ");
+       System.out.println(this.portegarage + "se ferme à 50%");
+      
+    }*/
+    public boolean verouiller
+    {
+    
+        if (deverouiller==true)
+        {
+            deverouiller==false;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    public boolean deverouiller()
+    {
+        if (verouille==true)
+        {
+            verouille=false;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
