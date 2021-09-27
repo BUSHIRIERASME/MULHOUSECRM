3ec976f306b482ea2fda1274cbb588520ab7ee26
BUSHIRIERASME <85500868+BUSHIRIERASME@users.noreply.github.com>
2021-09-27 17:00:46 +0200

    T270921a

--- src/JAVA SCRIPT/Saisie_utilisateur.js
+++ src/JAVA SCRIPT/Saisie_utilisateur.js
@@ -7,11 +7,11 @@ let nom="";
 let prenom="";
 let age="";
  nom=prompt("quel est votre nom ?");
-document.write(nom)
+document.write(nom + "  ")
 // console.log(nom +" ");
 prenom=prompt("quel est votre prenom ?");
-document.write(prenom)
+document.write(prenom + "  ")
 // console.log(prenom + " ");
 age=prompt("quel est votre age ?");
 document.write(age)
-console.log(nom=  " " , prenom =  " "  ,age = " ");
+console.log(nom+  " " + prenom +  " "  +age + " ");
