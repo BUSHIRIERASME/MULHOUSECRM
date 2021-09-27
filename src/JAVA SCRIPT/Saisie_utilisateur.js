e8a60eb4800fa94565fba56d3c239ec641fec260
BUSHIRIERASME <85500868+BUSHIRIERASME@users.noreply.github.com>
2021-09-27 16:58:08 +0200

    T270921

---
+++ src/JAVA SCRIPT/Saisie_utilisateur.js
@@ -0,0 +1,17 @@
+// let nom = "Christophe" ;
+// let prenom = "Jean";
+// let age = 24 ;
+// console.log(nom + "   " + prenom + "  " +age)
+
+let nom="";
+let prenom="";
+let age="";
+ nom=prompt("quel est votre nom ?");
+document.write(nom)
+// console.log(nom +" ");
+prenom=prompt("quel est votre prenom ?");
+document.write(prenom)
+// console.log(prenom + " ");
+age=prompt("quel est votre age ?");
+document.write(age)
+console.log(nom=  " " , prenom =  " "  ,age = " ");
--- src/JAVA SCRIPT/inversion2.js
+++ src/JAVA SCRIPT/inversion2.js
@@ -1,12 +1,12 @@
-let var1 = 5;
-let var2 = 7;
-let var3 = 3;
-var2=[var1,var1=var2][0];
-console.log(var1,var2, var3);
-console.log(var2,var3,var1);
-console.log(var3,var1,var2);
-var1=[var2,var2=var1][0];
-console.log(var2,var1);
+// let var1 = 5;
+// let var2 = 7;
+// let var3 = 3;
+// var2=[var1,var1=var2][0];
+// console.log(var1,var2, var3);
+// console.log(var2,var3,var1);
+// console.log(var3,var1,var2);
+// var1=[var2,var2=var1][0];
+// console.log(var2,var1);
 
 // let a = 5, b = 6;
 // [a, b] = [b, a];
@@ -14,6 +14,14 @@ console.log(var2,var1);
 // var a = 1,
 //     b = 2;
 
-// [a, b] = [b, a];
+let a=10;
+let b=20;
+console.log(a,b);
+[a,b]=[b,a]
+console.log(a,b);
 
-// console.log('a:', a, 'b:', b); // a: 2 b: 1
+// let x,y;
+// [x,y]=[2,3]
+// console.log(x,y);
+// [y,x]=[3,2];
+// console.log(3,2);
