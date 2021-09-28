b961a559be17be2c8f5d70da0425bd47094dc970
BUSHIRIERASME <85500868+BUSHIRIERASME@users.noreply.github.com>
2021-09-28 16:53:47 +0200

    T280921b

--- src/JAVA SCRIPT/Differentes_phrases.js
+++ src/JAVA SCRIPT/Differentes_phrases.js
@@ -1,18 +1,28 @@
-let sujet;
-let verbe;
-let complement;
-let phrase;
+var sujet;
+var verbe;
+var complement;
+var phrase;
 
-sujet=prompt("Entrer le sujet");
-document.write("le sujet est:" + sujet );
+sujet=prompt("Entrer le sujet ");
+document.write("<p>le sujet est:" + sujet );
 
-verbe=prompt(" Entrer le verbe est:");
-document.write("le verbe est:" + verbe );
+verbe=prompt(" Entrer le verbe: ");
+document.write("<p>le verbe est: " + verbe );
 
-complement=prompt("Entrer le complement:");
-document.write(" le complement:" + complement);
+complement=prompt("Entrer le complement est: ");
+document.write("<p>le complement est: "  + complement );
 
-phrase = sujet + verbe + complement;
-phrase=prompt("la phrase est:");
+ phrase = (sujet+verbe+complement);
+document.write(sujet+verbe+complement);
 
 
+
+
+// var chaine1 = "Monsieur Roger ";
+// var chaine2 = "feras son discour";
+// var chaine3 = "à la fin du mois.";
+// var chaine4 = chaine1+chaine2+chaine3;
+//  console.log(chaine1+chaine2+chaine3);
+
+//  var titre = " Qu'il y a t-il dans cette maison";
+//  console.log(titre);
