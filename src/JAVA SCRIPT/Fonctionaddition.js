0e0dd91c2cc44a8689535165c2b05d07e176b337
BUSHIRIERASME <85500868+BUSHIRIERASME@users.noreply.github.com>
2021-10-01 11:43:34 +0200

    T011021

---
+++ src/JAVA SCRIPT/Fonctionaddition.js
@@ -0,0 +1,43 @@
+
+
+    // var number1=3;
+    // var number2=2;
+    // result=number1*number2
+    // alert(result);
+
+    // var number=3;
+    // number=number+5;
+    // alert(number);
+
+    // var age = parseInt(prompt('Quel est votre âge ?'));
+    // if (1 <= age && age <= 6) {
+    // alert('Vous êtes un jeune enfant.');
+    // } else if (7 <= age && age <= 11) {
+    // alert ('Vous êtes un enfant qui a atteint l\'âge de raison.');
+    // } else if (12 <= age && age <= 17) {
+    // alert ('Vous êtes un adolescent.');
+    // } else if (18 <= age && age <= 120) {
+    // alert ('Vous êtes un adulte.');
+    // } else {
+    // alert ('Erreur !!');
+    // }
+    
+    
+    // var number = 1;
+    // number++;
+    // alert(number); // Affiche : « 1 »
+    // number--;
+    // alert(number); // Affiche : « 0 »
+   
+    
+
+    // var number = 5;
+    // number--;
+    // alert(number); // Affiche : « 1 »
+    // number--;
+    // alert(number); // Affiche : « 0 »
+
+        var sayHello = function() {
+        alert('Bonjour !');
+        };
+        sayHello();
--- src/JAVA SCRIPT/Tableau1.js
+++ src/JAVA SCRIPT/Tableau1.js
@@ -1,29 +1,38 @@
-let MonTableau =[0,10,21,32,45,51,68,73,84,98];
-let reponse=prompt("Taper un nombre");
-MonTableau.length;
+// let MonTableau =[0,10,21,32,45,51,68,73,84,98];
+// let reponse=prompt("Taper un nombre");
+// MonTableau.length;
 
-for(i=0; i<MonTableau.length;i++)
-{
-if (  reponse==MonTableau[i])
-{
-document.write("la reponse est bonne = bravo");
-}
-else
-{ 
-document.write("le nombre est mauvais")
-}
- document.write(MonTableau[i]+" ");
-}
+// for(i=0; i<MonTableau.length;i++)
+// {
+// if (  reponse==MonTableau[i])
+// {
+// document.write("la reponse est bonne bravo");
+// }
+// else
+// { 
+// document.write("le nombre est erroné");
+// }
+//  document.write(MonTableau[i]+" ");
+// }
 
 // MonTableau[0]
 
 
 // for(i=0,i<10,i++)
-{
+// {
 
-}
+// }
 
 //  alert( MonTablbeau[2]);
-Array.push("10");
+// Array.push("10");
 
     // var MonTablbeau=new Array();
+
+//     var myArray = ['Rafael', 'Mathilde', 'Ahmed', 'Jérôme', 'Guillaume'];
+// // Le contenu se définit entre crochets, avec une virgule entre chaque valeur.
+// // La chaîne 'Rafael' correspond à l'indice 0, 'Mathilde' à l'indice 1...
+// alert(myArray[1]); // Affiche : « Laurence »
+
+var myArray = ['Rafael', 'Mathilde', 'Ahmed', 'Jérôme', 'Guillaume'];
+myArray[1] = 'Paul';
+alert(myArray[1,4]); // Affiche : « Paul »
