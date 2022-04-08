91908a48fa39048c74c7a2c453f9e917f2fefd74
BUSHIRIERASME <85500868+BUSHIRIERASME@users.noreply.github.com>
2022-04-08 11:47:31 +0200

    T080422

--- src/JAVA SCRIPT/TP1_html_css.js/employe.js
+++ src/JAVA SCRIPT/TP1_html_css.js/employe.js
@@ -5,21 +5,26 @@ fetch("https://arfp.github.io/tp/web/frontend/employees/employees.json")
      return response.json();
 })
 .then((données) =>{
-    data = données;
-    console.log(data)
+    data = données.data;
+    console.log(data);
 
 //console.log('full name de la ligne 3: + données[4].fullname');
-const datalistElement = document.getElementById('datalist_employe');
+const tbodyElement = document.getElementById('resultat');
 for (let i=0 ; i< data.length ; i ++)
 {
-    const optionElement = document.getElementById('option');
-    optionElement.textContent = data[i].employee_name;
-    optionElement.textContent.setAttribute('value',data[i].identifiant)
-    datalistElement.appendChild(optionElement);
+    console.log("test");
+    let row = document.createElement('tr');
+    let dataEmployeeName = document.createElement('td');
+    dataEmployeeName.textContent = data[i].employee_name;
+    row.appendChild(dataEmployeeName);
+    tbodyElement.appendChild(row);
+    // optionElement.textContent = data[i].employee_name;
+    // optionElement.textContent.setAttribute('value',data[i].identifiant)
+    // datalistElement.appendChild(optionElement);
 }
 })
 .catch(error =>(error));
-btn.addEventListener('click',afficherResultatRechercher);
+// btn.addEventListener('click',afficherResultatRechercher);
 function afficherResultatRechercher(event){
     event.preventDefault();
     const value = document.getElementById('identifiant').value;
