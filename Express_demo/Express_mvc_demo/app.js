56350c6f41f3bba8ca6cf5f621fa9da3135edc4e
BUSHIRIERASME <85500868+BUSHIRIERASME@users.noreply.github.com>
2022-04-20 16:55:23 +0200

    T200422

---
+++ Express_demo/Express_mvc_demo/app.js
@@ -0,0 +1,13 @@
+// import d'express 
+const express = require('express');
+// application express
+const app = express()
+
+
+
+
+
+
+app.listen (80,() => {
+    console.log("sevreur esrt prêt (http://localhost)")
+});
