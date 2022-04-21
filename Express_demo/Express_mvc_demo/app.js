25e6825bd5b585dd1e6b86bc0b7981fc1f396ec4
BUSHIRIERASME <85500868+BUSHIRIERASME@users.noreply.github.com>
2022-04-21 15:51:50 +0200

    T21/04/22

--- Express_demo/Express_mvc_demo/app.js
+++ Express_demo/Express_mvc_demo/app.js
@@ -1,13 +1,32 @@
 // import d'express 
+const { response } = require('express');
 const express = require('express');
 // application express
-const app = express()
+const app = express();
+app.use((req,res,next)=>{
+    let method = req.method;
+    let path = req.originalUrl
+    console.log('${method} ${path}');
+    next()
+})
 
+app.get('/',(requete,response) => {
+    response.send('Accueil'); 
+})
 
+//creation de app
+app.get('/about', (req,res)=>{
+    response.send('A propos') 
+})
 
+// creation de la route,pour recuperer une partie de route 
 
-
+app.get('/hello/:name ?',(req,res) => {
+  let name = req.params.name ?? 'toto'
+  res.send('Bonjour${name}') 
+})
 
 app.listen (80,() => {
     console.log("sevreur esrt prêt (http://localhost)")
 });
+
