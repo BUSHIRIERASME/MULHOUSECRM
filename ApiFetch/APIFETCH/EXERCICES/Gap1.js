e24a60cbf408aa2f8e9e93793bf430c39c1ea241
BUSHIRIERASME <erasme.bushiri@hotmail.fr>
2022-06-08 19:38:57 +0200

    T080622

---
+++ ApiFetch/APIFETCH/EXERCICES/Gap1.js
@@ -0,0 +1,7 @@
+var container = 
+document.querySelector('.container'),
+slider = 
+document.querySelector('input');
+slider.addEventListener('input',function(){
+    container.style.gap = this.value + 'rem';
+}, false);
---
+++ ApiFetch/APIFETCH/Zipecod.js
@@ -0,0 +1,3 @@
+fetch("https://www.leparisien.fr/guide-shopping/les-13-navigateurs-web-les-plus-populaires-23-07-2019-8121987.php")
+.then(reponse => reponse.json)
+.then( reponse =>console.log)
